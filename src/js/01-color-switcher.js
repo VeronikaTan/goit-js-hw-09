8657807bb9499eba5346dd93025b184387398749
VeronikaTan <90012301+VeronikaTan@users.noreply.github.com>
2022-01-20 16:48:58 -0800

    finished 1st part of hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+const buttonStartEl = document.querySelector('button[data-start]')
+const stopEl = document.querySelector('button[data-stop]')
+const bodyEl = document.querySelector('body')
+
+let frequentEvent = null
+
+
+// bodyEl.addEventListener('change', )
+
+stopEl.disabled = true
+
+buttonStartEl.addEventListener('click', () => {
+
+frequentEvent = setInterval(() => {bodyEl.style.background = getRandomHexColor()}, 1000)
+   buttonStartEl.disabled = true
+    stopEl.disabled = false
+});
+    
+
+stopEl.addEventListener('click', () => {
+    clearInterval(frequentEvent)
+    
+    stopEl.disabled = true
+    buttonStartEl.disabled = false
+})
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    
+}console.dir(buttonStopEl)
