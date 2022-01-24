3771757d26e9608e894f79c083646b535b0bb261
VeronikaTan <90012301+VeronikaTan@users.noreply.github.com>
2022-01-24 10:06:22 -0800

    finished 3rd one

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const delayEl = document.querySelector('[name="delay"]')
+// const stepEl = document.querySelector('[name="step"]')
+// const amountEl = document.querySelector('[name="amount"]')
+
+const form = document.querySelector('.form');
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  delay = Number(delay.value);
+  step = Number(step.value);
+  amount = Number(amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      });
+    delay += step;
+  }
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
 }
